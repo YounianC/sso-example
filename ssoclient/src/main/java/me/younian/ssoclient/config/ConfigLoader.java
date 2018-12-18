86565eb509b6f234baec78d391919f4c2c29d4bc
Luxiaochuan <luxiaochuan@zhaoniuw.com>
2018-12-18 16:34:28 +0800

    server url 可配置

--- ssoclient/src/main/java/me/younian/ssoclient/config/ConfigLoader.java
+++ ssoclient/src/main/java/me/younian/ssoclient/config/ConfigLoader.java
@@ -1,8 +1,40 @@
 package me.younian.ssoclient.config;
 
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Properties;
+
+@Component
 public class ConfigLoader {
 
-    public static final String redirectUrl = "http://server.com:8080/login";
-    public static final String authCheckUrl = "http://server.com:8080/authCheck";
+    public static String applicationName = "";
+    public static String redirectUrl = "";
+    public static String authCheckUrl = "";
 
+    static {
+        try {
+            URL url = new URL("classpath:/sso-config.properties");
+            InputStream inStream = url.openStream();
+
+            Properties prop = new Properties();
+
+            System.out.println(prop.toString());
+
+            prop.load(inStream);
+            String server = prop.getProperty("ServerUrl");
+            redirectUrl = server + "/login";
+            authCheckUrl = server + "/authCheck";
+            applicationName = prop.getProperty("ApplicaitonName");
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
